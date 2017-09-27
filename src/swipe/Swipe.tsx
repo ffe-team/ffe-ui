d105e549bd1f2a8d722e0089cfef6ed841235a98
hore <chenhao@kedouinc.com>
2017-09-27 18:07:28 +0800

    【修改】删除video，新增swipe

---
+++ src/swipe/Swipe.tsx
@@ -0,0 +1,62 @@
+/**
+ * @desc: Swipe container
+ * @author: ssruoyan
+ * @create: 2017-09-06 17:42:39
+ * @modified by: ssruoyan
+ * @modified time: 2017-09-06 17:42:39
+ */
+
+import * as React from 'react'
+import * as classnames from 'classnames'
+
+interface SwipePropTypes {
+  className?: string,
+  navigation: boolean,
+  autoPlay: boolean,
+  autoPlaySpeed: number,
+  pagination: boolean,
+  direction: 'horizontal' | 'vertical',
+  loop: boolean,
+  speed: number,
+  initialSlide: number,
+
+  onTransitionStart?: (swipe: any) => void,
+  onTransitionEnd?: (swipe: any) => void,
+  onSwipeChangeStart?: (swipe: any) => void,
+  onSwipeChangeEnd?: (swipe: any) => void
+  onDestroy?: (swipe: any) => void,
+  onAutoPlay?: (swipe: any) => void,
+}
+
+class Swipe extends React.Component<SwipePropTypes, any> {
+
+  public static defaultProps : SwipePropTypes = {
+    autoPlay: false,
+    autoPlaySpeed: 300,
+    navigation: false,
+    pagination: false,
+    speed: 300,
+    direction: 'horizontal',
+    loop: false,
+    initialSlide: 0,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+  move() {
+
+  }
+  translate() {
+
+  }
+  render() {
+    
+  }
+  
+
+}
--- src/video/components/control-bar/PlaybackRate.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import * as classnames from 'classnames'
-
-class PlaybackRate extends React.Component<any, any> {
-  constructor(props) {
-    super(props)
-  }
-}
